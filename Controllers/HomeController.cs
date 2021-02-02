0d019ba27f76845769064df3de7fa16cdb837a65
ComputerUser158 <oneringraw@gmail.com>
2021-02-01 19:27:44 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstResponsiveWebAppCollins.Models;
+
+namespace FirstResponsiveWebAppCollins.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FV = 0;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Class model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FV = model.AgeThisYear();
+
+            }
+            else
+            {
+                ViewBag.FV = 0;
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Models/Class.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstResponsiveWebAppCollins.Models
+{
+    public class Class
+    {
+        [Required(ErrorMessage = "Please enter a name.")]
+        public string Name { get; set; } // gets/sets name
+        [Required]
+        [Range(1900, 2021, ErrorMessage = "Number must be between 1900 and 2021.")]
+        public int YearOfBirth { get; set; } // gets/sets Year of birth
+
+        public int AgeThisYear()
+        {
+            int currentYear = DateTime.Now.Year;
+            int age = 0;
+            age = currentYear - YearOfBirth;
+            return age;
+        }
+    }
+}
